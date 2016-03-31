1ab3f0a921a8fc7839c582f127fe1d3d63a89f8a
pragkirk <pragkirk@kirkk.com>
2016-03-31 18:24:37 -0500

    Updated build
    Updated build and service for Heroku deploy.

--- src/main/java/com/kirkk/hello/HelloSpark.java
+++ src/main/java/com/kirkk/hello/HelloSpark.java
@@ -6,6 +6,7 @@ import static com.kirkk.hello.JsonUtil.*;
  
 public class HelloSpark {
     public static void main(String[] args) {
+    	port(getHerokuAssignedPort());
         get("/hello", (req, res) -> "Hello, World!");
         get("/loan", "application/json", (req, res) -> {
         	String principle = req.queryParams("principle");
@@ -20,5 +21,13 @@ public class HelloSpark {
         /*get("/loan", (req, res) -> {        	
         	return new Payment().setPayment(new LoanCalculator().calculatePayment("15000","6.0","60"));
         }, json());*/
+    }
+    
+    static int getHerokuAssignedPort() {
+        ProcessBuilder processBuilder = new ProcessBuilder();
+        if (processBuilder.environment().get("PORT") != null) {
+            return Integer.parseInt(processBuilder.environment().get("PORT"));
+        }
+        return 4567; //return default port if heroku-port isn't set (i.e. on localhost)
     }
 }
